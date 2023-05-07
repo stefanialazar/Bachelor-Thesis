7f0e24bc29518dd7050e8210f1c1a232086f72d6
stefanialazar <stefania.d.lazar@gmail.com>
2023-05-07 23:20:28 +0300

    comments&replies update

--- backend/IvyLakes/DTOs/CommentsReplyDTO.cs
+++ backend/IvyLakes/DTOs/CommentsReplyDTO.cs
@@ -9,12 +9,10 @@ namespace IvyLakes.DTOs
 {
     public class CommentsReplyDTO 
     {
-        public int ReplyId { get; set; }
         public Guid UserId { get; set; }
         public int CommentId { get; set; }
         public string CommentBody { get; set; }
         public string CommentImageUrl { get; set; }
-        public int? Score { get; set; }
 
     }
 }
--- frontend/MerchShop/src/app/track-series-episodes/track-series-episodes.component.ts
+++ frontend/MerchShop/src/app/track-series-episodes/track-series-episodes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RequestService } from '../core/request.service';
+import { HttpHeaders } from '@angular/common/http';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-track-series-episodes',
@@ -12,20 +14,23 @@ export class TrackSeriesEpisodesComponent implements OnInit {
   episodeNumber!: number;
   comments: any;
   commentsReplies: any[] = [];
+  userId: any;
+  seriesId: any;
+  message: { type: string, text: string } | null = null;
 
   constructor(private route: ActivatedRoute, private reqS: RequestService,) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const seasonEpisode = params['seasonepisode'];
-      var seriesId = params['id'];
+      this.seriesId = params['id'];
       const matches = seasonEpisode.match(/season(\d+)episode(\d+)/i);
 
       if (matches) {
         this.seasonNumber = parseInt(matches[1], 10);
         this.episodeNumber = parseInt(matches[2], 10);
-        this.reqS.get('https://localhost:44341/api/comments/' + seriesId + '/season' + this.seasonNumber + 'episode' + this.episodeNumber).subscribe((res: any) => {
-        this.comments = res;
+        this.reqS.get('https://localhost:44341/api/comments/' + this.seriesId + '/season' + this.seasonNumber + 'episode' + this.episodeNumber).subscribe((res: any) => {
+        this.comments = res.reverse();
         this.processReplies();
          });
       }
@@ -74,6 +79,115 @@ export class TrackSeriesEpisodesComponent implements OnInit {
   dislikeReply(reply: any): void {
     reply.score--;
   }
+
+  async submitComment(commentBody: string, commentImageUrl: string): Promise<void> {
+    const token: any= localStorage.getItem("jwt");
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    if(token){
+      const tokenObject = this.decodeToken(token);
+      this.userId = tokenObject.id;
+
+      if (!commentBody.trim()) {
+        console.log('Comment body cannot be empty.');
+        this.message = { type: 'warning', text: 'Comment body cannot be empty.' };
+        return;
+      }
+  
+      const imageUrlRegex = /\.(jpeg|jpg|gif|png|webp|svg|bmp)$/i;
+      if (!commentImageUrl.match(imageUrlRegex) && commentImageUrl) {
+        console.log('Invalid image URL.');
+        this.message = { type: 'warning', text: 'Invalid image URL.' };
+        return;
+      }
+  
+      const createCommentDTO = {
+        UserId: this.userId,
+        SeriesId: this.seriesId,
+        SeasonNumber: this.seasonNumber,
+        EpisodeNumber: this.episodeNumber,
+        CommentBody: commentBody,
+        CommentImageURL: commentImageUrl,
+      };
+    
+      try {
+        const response = await this.reqS.post('https://localhost:44341/api/comments/create', createCommentDTO, { headers }).toPromise();
+        console.log('Comment submitted:', response);
+        this.message = { type: 'success', text: 'Comment submitted.' };
+      } catch (error) {
+        console.error('Error submitting comment:', error);
+        this.message = { type: 'danger', text: 'Error submitting comment.' };
+      }
+    } else {
+      this.message = { type: 'warning', text: 'You need to be logged in to submit a comment.' };
+      return;
+    } 
+  }
+
+  async submitReply(replyBody: string, replyImageUrl: string, replyCommentId: number){
+
+    const token: any= localStorage.getItem("jwt");
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    if(token){
+      const tokenObject = this.decodeToken(token);
+      this.userId = tokenObject.id;
+
+      if (!replyBody.trim()) {
+        console.log('Reply body cannot be empty.');
+        this.message = { type: 'warning', text: 'Reply body cannot be empty.' };
+        return;
+      }
+  
+      const imageUrlRegex = /\.(jpeg|jpg|gif|png|webp|svg|bmp)$/i;
+      if (!replyImageUrl.match(imageUrlRegex) && replyImageUrl) {
+        console.log('Invalid image URL.');
+        this.message = { type: 'warning', text: 'Invalid image URL.' };
+        return;
+      }
+  
+      const createReplyDTO = {
+        UserId: this.userId,
+        CommentId: replyCommentId,
+        CommentBody: replyBody,
+        CommentImageURL: replyImageUrl,
+      };
+    
+      try {
+        const response = await this.reqS.post('https://localhost:44341/api/comments/reply/create', createReplyDTO, { headers }).toPromise();
+        console.log('Comment submitted:', response);
+        this.message = { type: 'success', text: 'Reply submitted.' };
+      } catch (error) {
+        console.error('Error submitting comment:', error);
+        this.message = { type: 'danger', text: 'Error submitting reply.' };
+      }
+    } else {
+      this.message = { type: 'warning', text: 'You need to be logged in to submit a reply.' };
+      return;
+    } 
+
+  }
+
+  decodeToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch(Error) {
+      return null;
+    }
+  }
+
+  onMessageClosed() {
+    this.message = null;
+  } 
+
 
 }
 
